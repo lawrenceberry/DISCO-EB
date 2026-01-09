9f04f66e507a9a100220c709ef4f39a38a987cd1
Lawrence Berry <lawrenceberry@outlook.com>
2026-01-09 14:45:05 +0000

    Tune profiler options

--- scripts/profile_CMB.py
+++ scripts/profile_CMB.py
@@ -1,13 +1,9 @@
-
-
-import os
-os.environ['CUDA_CUPTI_ACTIVITY_BUFFER_SIZE'] = '128'
-
 import time
 
 import jax
 jax.config.update("jax_enable_x64", True)
 jax.config.update('jax_platform_name', 'gpu')
+from jax.profiler import ProfileOptions
 
 from discoeb.cmb import compute_Cell_spectrum_from_cosmo_params
 from tests.test_cmb import STANDARD_COSMOLOGIES
@@ -26,8 +22,15 @@ print("STARTING COMPUTATION...")
 
 start = time.time()
 
-with jax.profiler.trace("./profile_CMB_trace"):
-    Cell, param = compute_Cell_spectrum_from_cosmo_params(cosmo_params, ellmax=50)
+options = ProfileOptions()
+options.advanced_configuration = {
+    'gpu_max_callback_api_events': 8*1024*1024,
+    'gpu_max_activity_api_events': 8*1024*1024,
+    'gpu_pm_sample_buffer_size_per_gpu_mb': 128,
+    'gpu_pm_sample_interval_us': 20000,
+}
+with jax.profiler.trace("./profile_CMB_trace", profiler_options=options):
+    Cell, param = compute_Cell_spectrum_from_cosmo_params(cosmo_params, ellmax=1)
     Cell.block_until_ready()
 
 end = time.time()
