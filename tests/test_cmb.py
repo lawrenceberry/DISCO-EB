cd5c871995790e69ee1757dc457d28a6a02a7fb6
Lawrence Berry <lawrenceberry@outlook.com>
2026-01-09 13:08:40 +0000

    Test cl benchmark only on ellmax=50 to speed up tests

--- tests/test_cmb.py
+++ tests/test_cmb.py
@@ -462,7 +462,7 @@ class TestCMBSpectrum:
         cosmo_params = STANDARD_COSMOLOGIES[cosmology_name]
 
         # Benchmark the computation, making sure the function is compiled first
-        benchmark.pedantic(compute_Cell_spectrum_from_cosmo_params, kwargs=dict(param_dict=cosmo_params, ellmax=2500), rounds=1, warmup_rounds=1)
+        benchmark.pedantic(compute_Cell_spectrum_from_cosmo_params, kwargs=dict(param_dict=cosmo_params, ellmax=50), rounds=1, warmup_rounds=1)
 
         # Load performance baseline
         baseline_file = "tests/resources/cmb_performance_baseline.json"
